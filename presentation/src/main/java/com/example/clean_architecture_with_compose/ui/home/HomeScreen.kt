60bdbd265e54e413c3e09ad5f444a548fcbc7df7
Joonseo Park <jspark2015@unist.ac.kr>
2023-06-01 00:37:15 +0900

    initial commit

--- presentation/src/main/java/com/example/clean_architecture_with_compose/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/example/clean_architecture_with_compose/ui/home/HomeScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
